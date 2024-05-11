537faf3e314b68006a91e56b51a0673001408815
AMAN DHOMNE <107036303+Aman-Dhomne@users.noreply.github.com>
2024-05-12 03:34:11 +0530

    Add files via upload

---
+++ Angle Tracker/main.py
@@ -0,0 +1,40 @@
+import cv2
+import numpy as np
+import time
+import PoseModule as pm
+
+cap = cv2.VideoCapture('boxing.mp4')
+
+detector = pm.poseDetector()
+count = 0
+dir = 0
+pTime = 0
+while True:
+    success, img = cap.read()
+    img = cv2.resize(img, (640, 480))
+    #img = cv2.resize(img, (1280, 720))
+    # img = cv2.imread("AiTrainer/test.jpg")
+    img = detector.findPose(img, False)
+    lmList = detector.findPosition(img, False)
+    # print(lmList)
+    if len(lmList) != 0:
+        # Right Arm
+        angle = detector.findAngle(img, 12, 14, 16)
+        # # Left Arm
+        #angle = detector.findAngle(img, 11, 13, 15,False)
+        per = np.interp(angle, (210, 310), (0, 100))
+        bar = np.interp(angle, (220, 310), (650, 100))
+        # print(angle, per)
+
+
+
+
+
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+    cv2.putText(img, str(int(fps)), (50, 100), cv2.FONT_HERSHEY_PLAIN, 5,
+                (255, 0, 0), 5)
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
